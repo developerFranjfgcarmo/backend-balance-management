ddad7dcddc541ae930064b4612d73a0ecbd0fa26
Francisco J Fernández <fjfernandez@salutic.es>
2020-06-10 23:49:39 +0200

    Created the base class to the domain classes

---
+++ BalanceManagement.Domain/DomainBase.cs
@@ -0,0 +1,44 @@
+﻿using BalanceManagement.Data.Context;
+using System;
+using System.Threading.Tasks;
+
+namespace BalanceManagement.Domain
+{
+    public class DomainBase : IDisposable
+    {
+        public DomainBase(IBalanceManagementDbContext balanceManagementDbContext)
+        {
+            BalanceManagementDbContext = balanceManagementDbContext;
+        }
+        public IBalanceManagementDbContext BalanceManagementDbContext { get; set; }
+
+        protected bool SaveChanges()
+        {
+            return BalanceManagementDbContext.SaveChanges() > 0;
+        }
+
+        protected async Task<bool> SaveChangesAsync()
+        {
+            return await BalanceManagementDbContext.SaveChangesAsync() > 0;
+        }
+
+        #region [Disposable]
+        ~DomainBase()
+        {
+            Dispose(false);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+                BalanceManagementDbContext.Dispose();
+        }
+
+        public void Dispose()
+        {
+            Dispose(false);
+        }
+        #endregion
+
+    }
+}
