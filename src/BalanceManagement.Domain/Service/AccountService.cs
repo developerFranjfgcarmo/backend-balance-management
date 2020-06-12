f7627512ec593dd5ba8763271ac01bcf9deb3682
Francisco J Fernández <fjfernandez@salutic.es>
2020-06-12 11:06:20 +0200

    Added AccountService

--- src/BalanceManagement.Domain/Service/AccountService.cs
+++ src/BalanceManagement.Domain/Service/AccountService.cs
@@ -1,10 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using BalanceManagement.Contracts.Dtos;
+using BalanceManagement.Contracts.Dtos.Filter;
+using BalanceManagement.Data.Context;
+using BalanceManagement.Service.IService;
 
 namespace BalanceManagement.Service.Service
 {
-    public class AccountService
+    public class AccountService:ServiceBase, IAccountService
     {
+        public AccountService(IBalanceManagementDbContext balanceManagementDbContext) : base(balanceManagementDbContext)
+        {
+        }
+
+        public Task<AccountDto> AddAsync(AccountDto accountDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AccountDto> UpdateAsync(AccountDto accountDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AccountDto> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AccountDetailsDto> GetBalanceByAccountAsync(AccountFilter accountFilter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ModifyBalanceAsync(ModifyBalanceDto modifyBalance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> BalanceTransferToUser(BalanceTransferDto balanceTransfer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
